3cbf35021fd4f6302e71931f61424c06685bfe33
Bruce <leeharrison1984@gmail.com>
2021-06-03 12:19:27 -0500

    create services

--- scripts/services.sh
+++ scripts/services.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo -e "\n-= Create Block Producer Service =-"
-cat > ${NODE_HOME}/cardano-block-producer.service << EOF 
+sudo sh -c "cat <<EOF > /etc/systemd/system/cardano-block-producer.service
 # The Cardano node service (part of systemd)
 # file: /etc/systemd/system/cardano-block-producer.service 
 
@@ -17,7 +17,6 @@ Type            = simple
 WorkingDirectory= ${NODE_HOME}
 ExecStart       = /bin/bash -c '${NODE_HOME}/scripts/start-block-producer.sh'
 KillSignal=SIGINT
-RestartKillSignal=SIGINT
 TimeoutStopSec=2
 LimitNOFILE=32768
 Restart=always
@@ -26,12 +25,11 @@ SyslogIdentifier=cardano-block-producer
 
 [Install]
 WantedBy	= multi-user.target
-EOF
-sudo mv $NODE_HOME/cardano-block-producer.service /etc/systemd/system/cardano-block-producer.service
+EOF"
 sudo chmod 644 /etc/systemd/system/cardano-block-producer.service
 
 echo -e "\n-= Create Relay Service =-"
-cat > ${NODE_HOME}/cardano-relay.service << EOF 
+sudo sh -c "cat <<EOF > /etc/systemd/system/cardano-relay.service
 # The Cardano node service (part of systemd)
 # file: /etc/systemd/system/cardano-relay.service 
 
@@ -46,7 +44,6 @@ Type            = simple
 WorkingDirectory= ${NODE_HOME}
 ExecStart       = /bin/bash -c '${NODE_HOME}/scripts/start-relay.sh'
 KillSignal=SIGINT
-RestartKillSignal=SIGINT
 TimeoutStopSec=2
 LimitNOFILE=32768
 Restart=always
@@ -55,11 +52,11 @@ SyslogIdentifier=cardano-relay
 
 [Install]
 WantedBy	= multi-user.target
-EOF
-sudo mv $NODE_HOME/cardano-relay.service /etc/systemd/system/cardano-relay.service
+EOF"
 sudo chmod 644 /etc/systemd/system/cardano-relay.service
 
-echo -e "\n-= Reload Systemctl and enable cardano-relay =-"
-echo -e "-= If this is to be a block-producer, you must manually enable the service =-"
+echo -e "\n-= Reload Systemctl and disable services =-"
+echo -e "-= You must manually enable the services after starting the image =-"
 sudo systemctl daemon-reload
 sudo systemctl disable cardano-relay
+sudo systemctl disable cardano-block-producer
