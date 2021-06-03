04fef794e736b4617bfe67f161f6c4097e3338e2
Bruce <leeharrison1984@gmail.com>
2021-06-03 17:29:51 -0500

    grant read privilages to the cwagent for system logs

--- scripts/cloudwatch.sh
+++ scripts/cloudwatch.sh
@@ -8,6 +8,10 @@ sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
 sudo cp ${HOME}/setup/monitoring_config/amazon-cloudwatch-agent.json ${CLOUDWATCH_CONFIG}
 sudo cp ${HOME}/setup/monitoring_config/prometheus.yml ${NODE_HOME}/config
 sudo yum install -y amazon-cloudwatch-agent
-sudo gpasswd --add cwagent adm
+sudo gpasswd --add cwagent cardano
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:${CLOUDWATCH_CONFIG}
 sudo systemctl enable amazon-cloudwatch-agent.service
+
+echo -e "\n-= Grant cwagent permission to read log streams =-"
+sudo setfacl -m u:cwagent:x /var/log/messages
+sudo setfacl -m u:cwagent:x /var/log/secure
