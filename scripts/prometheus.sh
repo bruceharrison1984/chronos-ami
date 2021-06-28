9fd0a7e6bd9694acd5839431f8836f78b3e9e0d9
Bruce <leeharrison1984@gmail.com>
2021-06-28 13:07:33 -0500

    add instance Id as metric dimension

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -14,6 +14,10 @@ chmod +x ${NODE_HOME}/scripts/prometheus-to-cloudwatch
 echo -e "\n-= Create Prometheus-To-Cloudwatch Startup Script =-"
 cat <<EOF > ${NODE_HOME}/scripts/start-prometheus-to-cloudwatch.sh
 #!/bin/bash
+
+RAW_ID=$(ec2-metadata --instance-id)
+INSTANCE_ID=${RAW_ID##instance-id:}
+
 export CLOUDWATCH_NAMESPACE=cardano-metrics
 export CLOUDWATCH_REGION=${TARGET_REGION}
 export CLOUDWATCH_PUBLISH_TIMEOUT=5
@@ -21,6 +25,7 @@ export PROMETHEUS_SCRAPE_INTERVAL=30
 export PROMETHEUS_SCRAPE_URL=http://localhost:12798/metrics
 export ACCEPT_INVALID_CERT=true
 export INCLUDE_METRICS=cardano_node_metrics_*
+export ADDITIONAL_DIMENSION="InstanceId=${INSTANCE_ID}"
 /usr/local/bin/prometheus-to-cloudwatch
 EOF
 chmod +x ${NODE_HOME}/scripts/start-prometheus-to-cloudwatch.sh
