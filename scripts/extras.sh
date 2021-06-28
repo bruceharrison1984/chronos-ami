fd50be3dadc9de9529241336be620df477693e13
Bruce <leeharrison1984@gmail.com>
2021-06-28 13:07:54 -0500

    fail2ban install moved to beginning of process

--- scripts/extras.sh
+++ scripts/extras.sh
@@ -5,19 +5,6 @@ echo -e "\n-= Install Google Authentication =-"
 echo -e "-= (This needs to be manually setup once you start a server with this image) =-"
 sudo yum -y install google-authenticator qrencode
 
-echo -e "\n-= Install Fail2Ban =-"
-sudo yum install -y fail2ban
-sudo systemctl enable fail2ban
-sudo sh -c 'cat <<EOF > /etc/fail2ban/jail.d/sshd.local
-[sshd]
-enabled = true
-port = ssh
-action = iptables-multiport
-logpath = /var/log/secure
-maxretry = 3
-bantime = 600
-EOF'
-
 echo -e "\n-= Setup MOTD =-"
 sudo sh -c 'cat <<EOF > /etc/update-motd.d/30-banner
 #!/bin/bash
