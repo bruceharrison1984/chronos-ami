96eaba9642cdfc9874464d8d9b7d09f9121718c7
Bruce <leeharrison1984@gmail.com>
2021-06-28 21:23:07 -0500

    fix prometheus script to send instance-id

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -16,8 +16,8 @@ cat <<EOF > ${NODE_HOME}/scripts/start-prometheus-to-cloudwatch.sh
 #!/bin/bash
 set -e
 
-RAW_ID="$(ec2-metadata --instance-id)"
-INSTANCE_ID=${RAW_ID##instance-id:}
+RAW_ID="\$(ec2-metadata --instance-id)"
+INSTANCE_ID="\${RAW_ID##instance-id:}"
 
 export CLOUDWATCH_NAMESPACE=cardano-metrics
 export CLOUDWATCH_REGION=${TARGET_REGION}
@@ -26,7 +26,7 @@ export PROMETHEUS_SCRAPE_INTERVAL=30
 export PROMETHEUS_SCRAPE_URL=http://localhost:12798/metrics
 export ACCEPT_INVALID_CERT=true
 export INCLUDE_METRICS=cardano_node_metrics_*
-export ADDITIONAL_DIMENSION="InstanceId=${INSTANCE_ID}"
+export ADDITIONAL_DIMENSION="InstanceId=\${INSTANCE_ID}"
 /usr/local/bin/prometheus-to-cloudwatch
 EOF
 chmod +x ${NODE_HOME}/scripts/start-prometheus-to-cloudwatch.sh
